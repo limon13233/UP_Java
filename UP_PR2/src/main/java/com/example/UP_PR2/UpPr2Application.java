7026c5c7e3106f6f14302cf3fdb05872d396afbe
limon13233 <91593740+limon13233@users.noreply.github.com>
2022-10-17 23:37:36 +0300

    pr2
    pr2

---
+++ UP_PR2/src/main/java/com/example/UP_PR2/UpPr2Application.java
@@ -0,0 +1,13 @@
+package com.example.UP_PR2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpPr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpPr2Application.class, args);
+	}
+
+}
